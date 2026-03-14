9d62766cd7a072dafcad5c67e013bef733a7caef
wlasitz-jeremy <wlasitz@gmail.com>
2026-03-14 17:51:29 -0600

    changed footer

--- Personal-project/scripts/scripts.js
+++ Personal-project/scripts/scripts.js
@@ -1,7 +1,5 @@
-const scubadiving =
-  "I love scuba diving! Scuba diving is an amazing experience.";
 const rockclimbing =
   "I love rock climbing! Rock climbing is an amazing experience.";
-const message = `${scubadiving} ${rockclimbing}`;
+const message = `${rockclimbing}`;
 screen = document.getElementsByClassName("footer");
 screen[0].textContent = message;
