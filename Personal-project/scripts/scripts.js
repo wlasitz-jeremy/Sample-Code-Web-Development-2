a4c8cf021d49062637c5428b82fbff33d32370d9
wlasitz-jeremy <wlasitz@gmail.com>
2026-03-14 18:09:35 -0600

    changed header and footer

--- Personal-project/scripts/scripts.js
+++ Personal-project/scripts/scripts.js
@@ -1,5 +1,8 @@
-const rockclimbing =
-  "I love rock climbing! Rock climbing is an amazing experience.";
-const message = `${rockclimbing}`;
-screen = document.getElementsByClassName("footer");
-screen[0].textContent = message;
+const water = "Scuba Diving is the best sport ever!";
+const message = `${water}`;
+const element = document.getElementById("water");
+element.textContent = message;
+const land = "Rock Climbing is the best sport ever!";
+const message2 = `${land}`;
+const element2 = document.getElementById("land");
+element2.textContent = message2;
