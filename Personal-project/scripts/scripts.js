9387d8aed3f5844233fc1082a068febc66245ba8
wlasitz-jeremy <wlasitz@gmail.com>
2026-03-17 10:29:12 -0600

    changed back

--- Personal-project/scripts/scripts.js
+++ Personal-project/scripts/scripts.js
@@ -1,7 +1,8 @@
 const water = "Scuba Diving is the best sport ever!";
-const message = `${water}`;
-const element = document.getElementById("water");
+const message = `${water} ${mess1}`;
+const element = document.getElementsByClassName("water");
 element.textContent = message;
+
 const land = "Rock Climbing is the best sport ever!";
 const message2 = `${land}`;
 const element2 = document.getElementById("land");
